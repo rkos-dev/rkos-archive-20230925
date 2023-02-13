5c3494666637382df0dabbf6239fe930af5db1ff
jxy <xyyy1420@gmail.com>
2023-02-13 23:37:54 +0800

    feat:新增chroot部分脚本
    Signed-off-by: jxy <xyyy1420@gmail.com>

---
+++ src/enter_chroot_scripts/prepare_vir_filesystem.sh
@@ -0,0 +1,9 @@
+mkdir -pv $LFS/{dev,proc,sys,run}
+mount -v --bind /dev $LFS/dev
+mount -v --bind /dev/pts $LFS/dev/pts
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
+if [ -h $LFS/dev/shm ]; then
+    mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+fi
