a25cd741772a46bbadc8ae18ec3909f34d1cea15
jxy <xyyy1420@gmail.com>
2023-02-16 00:19:33 +0800

    feat:新增一个取软件包路径的通用函数，预计改写为软件包安装的通用函数
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,9 @@ use std::fs::{File, OpenOptions};
 use std::io::Write;
 use std::os::unix::fs;
 use std::path::Path;
+use std::result::Result;
+
+use glob::glob;
 
 pub fn target_path_exists(path: &str) -> Result<(), Box<dyn Error>> {
     let target_path = Path::new(&path);
@@ -24,3 +27,17 @@ pub fn chroot(path: &str) -> std::io::Result<()> {
     std::env::set_current_dir("/")?;
     Ok(())
 }
+
+//TODO:后续修改为获取绝对路径
+//pub fn get_real_path(dir_path: String, package: String) -> Result<(), Box<dyn Error>> {
+//    let path = match glob(&(dir_path + &package))
+//        .unwrap()
+//        .filter_map(Result::ok)
+//        .next()
+//    {
+//        Some(v) => v,
+//        None => panic!("not pattern path"),
+//    };
+//
+//    Ok(())
+//}
