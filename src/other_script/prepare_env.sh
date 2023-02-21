79c0a818a10c75452346dfde82dc7f1805644592
jxy <xyyy1420@gmail.com>
2023-02-21 18:57:13 +0800

    fix:修改环境变量准备脚本，测试通过
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- src/other_script/prepare_env.sh
+++ src/other_script/prepare_env.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
 
 #默认加载LFS系统变量
-cp /root/.bash_profile{,.origin}
-echo "export LFS=/mnt/lfs" >> /root/.bash_profile
-source /root/.bash_profile
+cp /root/.bash_profile{,.LFS_origin}
+if [ $LFS ];then 
+    echo "LFS=$LFS" 
+else
+    echo "export LFS=/mnt/lfs" >> /root/.bash_profile
+fi
 
 #系统默认挂载硬盘，TODO:这部分硬盘设备名还得实际调整，如果可以动态是最好的
 mkdir -pv $LFS
-cp /etc/fstab{,.origin}
-echo "/dev/sdb1 /mnt/lfs ext4 defaults 1 1" >> /etc/fstab
+mkdir -pv $LFS/boot
+cp /etc/fstab{,.DISK_origin}
+echo "/dev/vdb1 /mnt/lfs/boot vfat defaults 1 1" >> /etc/fstab
+echo "/dev/vdb2 /mnt/lfs ext4 defaults 1 1" >> /etc/fstab
 mount -a
+systemctl daemon-reload
 
 #默认设定目标系统的环境变量，TODO:后续收尾需要替换名字
-cp /root/.bash_profile{,.origin}
-echo "export LFS_TGT=$(uname -m)-linux-gnu" >> /root/.bash_profile
-source /root/.bash_profile
+cp /root/.bash_profile{,.TGT_origin}
+if [ $LFS_TGT ]; then
+    echo "LFS_TGT=$LFS_TGT"
+else
+    echo "export LFS_TGT=$(uname -m)-linux-gnu" >> /root/.bash_profile
+fi
 
 #设定LC_ALL，PATH，CONFIG_FILE的环境变量
-cp /root/.bash_profile{,.origin}
-echo "export LC_ALL=POSIX" >> /root/.bash_profile
+cp /root/.bash_profile{,.PATH_origin}
+if [ $LC_ALL ];then
+    echo "LC_ALL=$LC_ALL"
+else
+    echo "export LC_ALL=POSIX" >> /root/.bash_profile
+fi
 
 PATH=/usr/bin
 if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
 PATH=$LFS/tools/bin:$PATH
 echo "export PATH=$PATH" >> /root/.bash_profile
 
-CONFIG_FILE=$LFS/usr/share/config.site
+if [ $CONFIG_FILE ];then
+    echo "CONFIG_FILE=$CONFIG_FILE"
+else
+    CONFIG_FILE=$LFS/usr/share/config.site
+fi
 echo "export CONFIG_FILE=$CONFIG_FILE" >> /root/.bash_profile
 
 CPUS=$(cat /proc/cpuinfo | grep "processor" | wc -l)
-echo "export MAKEFLAGS='-j$CPUS'" >> /root/.bash_profile
+if [ $MAKEFLAGS ];then
+    echo "MAKEFLAGS=$MAKEFLAGS"
+else
+    echo "export MAKEFLAGS='-j$CPUS'" >> /root/.bash_profile
+fi
 
 source /root/.bash_profile
 
