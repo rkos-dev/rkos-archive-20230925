eb37eb8c15bb5b93ef7ed90151d93c49ab858d75
jxy <xyyy1420@gmail.com>
2023-02-23 18:00:34 +0800

    feat:增加chroot之后基础的逻辑结构
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- src/build_lfs_sys.rs
+++ src/build_lfs_sys.rs
@@ -1,11 +1,14 @@
 extern crate dagrs;
 
 use dagrs::{init_logger, DagEngine, EnvVar, Inputval, Retval, TaskTrait, TaskWrapper};
+use std::collections::HashMap;
 use std::process::Command;
+use vars;
 
 struct InstallBasicSystemSoftware {}
 impl TaskTrait for InstallBasicSystemSoftware {
     fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        let mut package_install_list = HashMap::new();
         //按顺序安装软件包
         //在coreutils之前，安装clangd，llvm，rust
         //删除无用文件，保留调试符号
