19218437b1e6571d148df33683879ecbcfd99f11
jxy <xyyy1420@gmail.com>
2023-02-23 17:58:17 +0800

    fix:修复错误的链接导致编译过程中部分依赖无法找到的问题
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- src/other_script/create_dirs.sh
+++ src/other_script/create_dirs.sh
@@ -2,10 +2,10 @@ cd $LFS
 mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 
-mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+mkdir -pv $LFS/{etc,var}
 
 for i in bin lib sbin; do
-  ln -sv usr/$i $LFS/$i
+  ln -sv /usr/$i $LFS/$i
 done
 
 case $(uname -m) in
