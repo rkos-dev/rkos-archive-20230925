0e3f709ca6c67cf8c0ca70533846230f350371e9
jxy <xyyy1420@gmail.com>
2023-02-23 17:56:34 +0800

    fix:修复patch包路径问题
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- src/base_package_script/kbd.sh
+++ src/base_package_script/kbd.sh
@@ -1,4 +1,4 @@
-patch -Np1 -i ../kbd-2.5.1-backspace-1.patch
+patch -Np1 -i ../base_patches/kbd-2.5.1-backspace-1.patch
 
 sed -i '/RESIZECONS_PROGS=/s/yes/no/' configure
 sed -i 's/resizecons.8 //' docs/man/man8/Makefile.in
