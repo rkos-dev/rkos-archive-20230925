3804f59fee58ceaa6839530ad4b5f175a0ebc369
jxy <xyyy1420@gmail.com>
2023-03-19 23:31:08 +0800

    feat:新增cli支持
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use dagrs::{init_logger, DagEngine, EnvVar, Inputval, Retval, TaskTrait, TaskWra
 use log::{debug, error, info, trace, warn};
 use std::collections::HashMap;
 use std::env;
+use std::fs;
 
 mod build_lfs_sys;
 mod build_temp_toolchain;
@@ -10,6 +11,7 @@ mod prepare_host_sys;
 mod utils;
 mod vars;
 use cmd_lib::*;
+use std::path::Path;
 use std::path::PathBuf;
 
 struct OutputLfsImg {}
@@ -29,48 +31,134 @@ impl TaskTrait for CreateVmBack {
     }
 }
 
-//TODO:增加流程控制
-struct DagNodes {
-    prepare_env: bool,
-    prepare_disk: bool,
-    check_env: bool,
-    prepare_software: HashMap<String, bool>,
-    compile_cross_toolchain_check: bool,
-    compile_cross_toolchain: HashMap<String, bool>,
-    prepare_chroot_chmod: bool,
-    prepare_virt_sys: bool,
-    enter_chroot: bool,
-    create_path: bool,
-    compile_temp_packages: bool,
-    clean_up: bool,
-    build_basic_system: bool,
-    config_sys: bool,
-    output: bool,
+#[warn(dead_code)]
+fn init() {
+    let stop_flag_path = Path::new("./stop");
+    if stop_flag_path.exists() {
+        if let Err(e) = fs::remove_file(stop_flag_path) {
+            panic!("Cannot init stop flag {}", e);
+        }
+    }
 }
 
 fn main() {
+    init();
     init_logger(None);
-    let prepare = TaskWrapper::new(prepare_host_sys::Prepare {}, "Prepare");
-    let compile_temp_packages = TaskWrapper::new(
-        build_temp_toolchain::CompileTempPackages {},
-        "Compile temp packages",
+    let cli = vars::Cli::parse();
+
+    //检测是否在必要的工作目录下
+    let current_dir = env::current_dir().unwrap();
+    if current_dir.parent() != Some(Path::new("/mnt")) {
+        error!(
+            "Cannot run at path {:?} , you must take the program in /mnt/[TEMP_SYSTEM]",
+            current_dir
+        );
+        return;
+    }
+
+    //检查环境变量
+    let check_env = TaskWrapper::new(prepare_host_sys::CheckEnv {}, "Check host env");
+
+    //准备配置目标文件目录
+    let mut prepare_dirs = TaskWrapper::new(prepare_host_sys::PreparingDirs {}, "PREPARE DIRS");
+
+    //准备下载软件
+    let mut prepare_software =
+        TaskWrapper::new(prepare_host_sys::PreparingSoftware {}, "Prepare software");
+
+    //安装临时工具链
+    let mut compile_toolchains = TaskWrapper::new(
+        build_temp_toolchain::CompilingCrossToolChain {},
+        "Compile Toolchains",
     );
-    let enter_fakeroot = TaskWrapper::new(utils::EnterFakeroot {}, "Enter fakeroot");
-    let mut build_base_system =
-        TaskWrapper::new(build_lfs_sys::BuildBaseSystem {}, "Build base system");
-    //    let config_system=TaskWrapper::new(config_sys::ConfigSys{},"Config new system");
-    //let t1 = TaskWrapper::new(build_temp_toolchain::CompilingCrossToolChain {}, "Task 1");
 
-    //    let mut dag_nodes = vec![prepare, compile_temp_packages, build_base_system];
+    //进入chroot环境
+    let mut enter_chroot = TaskWrapper::new(utils::EnterChroot {}, "Enter Chroot");
 
-    build_base_system.exec_after(&[&enter_fakeroot]);
+    //配置进入chroot环境后的部分
+    let mut after_chroot =
+        TaskWrapper::new(build_temp_toolchain::AfterChroot {}, "After chroot config");
 
-    let mut dag_nodes = vec![enter_fakeroot];
+    //安装chroot后的工具
+    let mut install_other_packages = TaskWrapper::new(
+        build_temp_toolchain::AfterChrootInstall {},
+        "Install other packages",
+    );
 
-    ////let mut t2 = TaskWrapper::new(prepare_host_sys::PreparingDisk {}, "Task 2");
-    ////let mut t2 = TaskWrapper::new(prepare_host_sys::PreparingNewFileSystem {}, "task 2");
+    //清理系统
+    let mut clean_system = TaskWrapper::new(
+        build_temp_toolchain::CleanUpAndSaveTempSystem {},
+        "Clean up",
+    );
+
+    //安装基础软件包
+    let mut install_packages = TaskWrapper::new(
+        build_lfs_sys::InstallBasicSystemSoftware {},
+        "Install Packages",
+    );
+    //删除debug的符号
+    let mut remove_debug_symbol =
+        TaskWrapper::new(build_lfs_sys::RemoveDebugSymbol {}, "Remove debug symbol");
+    //清理系统
+    let mut clean_up_system = TaskWrapper::new(build_lfs_sys::CleanUpSystem {}, "Clean up system");
 
     let mut dagrs = DagEngine::new();
+    let mut dag_nodes: Vec<TaskWrapper> = Vec::new();
+
+    match &cli.build_option {
+        vars::BuildOption::Build => {
+            prepare_dirs.exec_after(&[&check_env]);
+
+            prepare_software.exec_after(&[&prepare_dirs]);
+
+            dag_nodes.push(check_env);
+
+            dag_nodes.push(prepare_dirs);
+
+            dag_nodes.push(prepare_software);
+        }
+
+        vars::BuildOption::HostConfig => {
+            prepare_dirs.exec_after(&[&check_env]);
+
+            dag_nodes.push(check_env);
+
+            dag_nodes.push(prepare_dirs);
+        }
+        vars::BuildOption::PackageDownload => {
+            dag_nodes.push(prepare_software);
+        }
+        vars::BuildOption::BuildTempToolchains => {
+            compile_toolchains.exec_after(&[&check_env]);
+            enter_chroot.exec_after(&[&compile_toolchains]);
+            after_chroot.exec_after(&[&enter_chroot]);
+            install_other_packages.exec_after(&[&after_chroot]);
+            clean_system.exec_after(&[&install_other_packages]);
+
+            dag_nodes.push(check_env);
+            dag_nodes.push(compile_toolchains);
+            dag_nodes.push(enter_chroot);
+            dag_nodes.push(after_chroot);
+            dag_nodes.push(install_other_packages);
+            dag_nodes.push(clean_system);
+        }
+        vars::BuildOption::BuildBackPackages => {
+            dag_nodes.push(install_packages);
+        }
+        vars::BuildOption::CleanUp => {
+            dag_nodes.push(remove_debug_symbol);
+        }
+        vars::BuildOption::ConfigTargetSystem => {}
+    }
+
+    match &cli.operate {
+        vars::StartMode::Start => {
+            dagrs.add_tasks(dag_nodes);
+        }
+        vars::StartMode::Reset => {}
+    };
+    assert!(dagrs.run().unwrap());
+
     ////TODO:python-doc需要调整包名，libstdc++需要调整包名，tcl-doc需要调整包名，zlib包会随着版本更新
     ////而链接失效，libstdc++只需要下载gcc之后copy一份成为libstdc++就可以
     ////python tcl 解决了 明天需要确认
@@ -78,39 +166,29 @@ fn main() {
     ////t2.exec_after(&[&t1]);
     ////t2.input_from(&[&t1]);
 
-    dagrs.add_tasks(dag_nodes);
-    assert!(dagrs.run().unwrap());
-
-    let current_dir = env::current_dir().unwrap();
-    info!("{:?}", current_dir);
+    //    dagrs.add_tasks(test_dag_nodes);
+    //    assert!(dagrs.run().unwrap());
 }
 
 #[cfg(test)]
 mod tests {
-    use std::env::current_dir;
-    use std::error::Error;
-    use std::ffi::OsStr;
-    use std::path::PathBuf;
-    use std::{env, fs};
-    fn test_walk_dir(path: PathBuf) -> Result<(), Box<dyn Error>> {
-        let current_dir = path;
-        println!("current_dir : {:?}", current_dir);
-        for entry in fs::read_dir(current_dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            let metadata = fs::metadata(&path)?;
-            if metadata.is_dir() {
-                if Some(OsStr::new(".git")) != path.file_name() {
-                    println!("{:?}", &path.file_name());
-                    test_walk_dir(path);
-                }
-            }
-        }
-        Ok(())
-    }
+    use crate::vars;
+
     #[test]
-    fn call_test_walk_dir() {
-        let current_dir = env::current_dir().unwrap();
-        test_walk_dir(current_dir);
+    fn const_vars_test() {
+        let root_dir = &vars::ROOT_DIR;
+        let base_config = &vars::BASE_CONFIG;
+        println!("{}", base_config.scripts_path.root.clone());
+        let all_package = &vars::ALL_PACKAGES;
+        println!("{}", all_package.all_packages[0].name.clone());
+        let cross_compile_packages = &vars::CROSS_COMPILE_PACKAGES;
+        println!(
+            "{}",
+            cross_compile_packages.cross_compile_packages[0]
+                .name
+                .clone()
+        );
+        let base_packages = &vars::BASE_PACKAGES;
+        println!("{}", base_packages.base_packages[0].name.clone());
     }
 }
