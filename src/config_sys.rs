de06d4906082617c73df3ad1a7b7b3aec1d327e0
jxy <xyyy1420@gmail.com>
2023-03-22 01:07:59 +0800

    feat:新增系统配置部分代码
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- src/config_sys.rs
+++ src/config_sys.rs
@@ -1,59 +1,241 @@
 extern crate dagrs;
 
-use dagrs::{init_logger, DagEngine, EnvVar, Inputval, Retval, TaskTrait, TaskWrapper};
-use std::process::Command;
+use crate::utils;
+use crate::utils::ProgramEndingFlag;
+use crate::vars;
+use dagrs::{init_logger, DagEngine, DagError, EnvVar, Inputval, Retval, TaskTrait, TaskWrapper};
+use log::info;
+use std::fs::File;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
 
-struct SystemConfiguration {}
-impl TaskTrait for SystemConfiguration {
+fn exec_config_script(script_path: PathBuf) -> bool {
+    let stdout_file = match File::create("/root/config.log") {
+        Ok(v) => v,
+        Err(_e) => return false,
+    };
+
+    let stderr_file = match stdout_file.try_clone() {
+        Ok(v) => v,
+        Err(_e) => return false,
+    };
+
+    let stdout = Stdio::from(stdout_file);
+    let stderr = Stdio::from(stderr_file);
+
+    let output = match Command::new("/bin/bash")
+        .env_clear()
+        .env("PATH", "/usr/bin:/usr/sbin")
+        .env("HOME", "/root")
+        .env("TERM", "$TERM")
+        .arg("-e")
+        .arg(script_path)
+        .stdout(stdout)
+        .stderr(stderr)
+        .status()
+    {
+        Ok(v) => v,
+        Err(_e) => return false,
+    };
+    output.success()
+}
+
+pub struct Fstab {}
+impl utils::ProgramEndingFlag for Fstab {}
+impl TaskTrait for Fstab {
     fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
-        //安装bootstrap
-        //处理udev
-        //管理设备
-        //配置网络
-        //配置systemvinit
-        //配置bash shell
-        //配置inputrc
-        //创建etc/shells
-        //
-        //bash
-        //dev
-        //inputrc
-        //linuxconsole
-        //NetworkConfig
-        //rc site
-        //shell config
-        //sysv boot
-        let hello_dagrs = String::from("Hello Dagrs!");
-        Retval::new(hello_dagrs)
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_fstab.sh",
+        ]
+        .iter()
+        .collect();
+        info! {"{:?}",script_path};
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+        Retval::empty()
     }
 }
 
-struct ManagerDev {}
-impl TaskTrait for ManagerDev {
-    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {}
+pub struct Inputrc {}
+impl utils::ProgramEndingFlag for Inputrc {}
+impl TaskTrait for Inputrc {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_inputrc.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+
+        Retval::empty()
+    }
 }
 
-struct NetworkConfig {}
-impl TaskTrait for NetworkConfig {
-    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {}
+pub struct Network {}
+impl utils::ProgramEndingFlag for Network {}
+impl TaskTrait for Network {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_network.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+
+        Retval::empty()
+    }
 }
 
-struct SysvInit {}
-impl TaskTrait for SysvInit {
-    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {}
+pub struct Profile {}
+impl utils::ProgramEndingFlag for Profile {}
+impl TaskTrait for Profile {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_profile.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+
+        Retval::empty()
+    }
 }
 
-struct BashShellConfig {}
-impl TaskTrait for BashShellConfig {
-    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {}
+pub struct RcSite {}
+impl utils::ProgramEndingFlag for RcSite {}
+impl TaskTrait for RcSite {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_rc_site.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+
+        Retval::empty()
+    }
 }
 
-struct CreateInputrc {}
-impl TaskTrait for CreateInputrc {
-    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {}
+pub struct Shell {}
+impl utils::ProgramEndingFlag for Shell {}
+impl TaskTrait for Shell {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_shell.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+
+        Retval::empty()
+    }
 }
 
-struct CreateShellConfig {}
-impl TaskTrait for CreateShellConfig {
-    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {}
+pub struct Sysvinit {}
+impl utils::ProgramEndingFlag for Sysvinit {}
+impl TaskTrait for Sysvinit {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_sysvinit.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+
+        Retval::empty()
+    }
+}
+
+pub struct Time {}
+impl utils::ProgramEndingFlag for Time {}
+impl TaskTrait for Time {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_time.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => {}
+            false => self.try_set_flag(false),
+        };
+
+        Retval::empty()
+    }
+}
+
+pub struct InstallKernel {}
+impl utils::ProgramEndingFlag for InstallKernel {}
+impl TaskTrait for InstallKernel {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        //TODO: 覆盖kernel配置，执行kernel编译脚本，移动kernel文件，结束
+
+        Retval::empty()
+    }
+}
+
+pub struct ConfigGrub {}
+impl utils::ProgramEndingFlag for ConfigGrub {}
+impl TaskTrait for ConfigGrub {
+    fn run(&self, _input: Inputval, _env: EnvVar) -> Retval {
+        self.check_flag();
+        //执行脚本
+        let script_path: PathBuf = [
+            &vars::BASE_CONFIG.scripts_path.root,
+            &vars::BASE_CONFIG.scripts_path.sysconfig,
+            "config_time.sh",
+        ]
+        .iter()
+        .collect();
+        match exec_config_script(script_path) {
+            true => (),
+            false => self.try_set_flag(false),
+        };
+        Retval::empty()
+    }
 }
