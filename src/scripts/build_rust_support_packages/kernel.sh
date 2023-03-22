561e611041e5b2fb9feab4152d799747b995dea4
jxy <xyyy1420@gmail.com>
2023-03-22 17:38:10 +0800

    feat:新增部分脚本
    Signed-off-by: jxy <xyyy1420@gmail.com>

---
+++ src/scripts/build_rust_support_packages/kernel.sh
@@ -0,0 +1,25 @@
+
+rustup component add rust-src
+
+cargo install --locked --version $(scripts/min-tool-version.sh bindgen) bindgen
+
+rustup component add rustfmt
+
+rustup component add clippy
+
+
+make CC=clang  rustavailable
+
+make CC=clang allnoconfig /config-6.1
+
+make CC=clang
+
+cp -iv arch/x86_64/boot/bzImage /boot/vmlinuz-6.1-rkos-0.0.1
+
+cp -iv System.map /boot/System.map-6.1
+
+cp -iv .config /boot/config-6.1
+
+
+install -d /usr/share/doc/linux-6.1
+cp -r Documentation/* /usr/share/doc/linux-6.1
