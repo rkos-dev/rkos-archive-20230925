2ff897165509f344bd18009f69962bba1d5b0765
jxy <xyyy1420@gmail.com>
2023-03-23 10:38:58 +0800

    fix:更新配置脚本
    Signed-off-by: jxy <xyyy1420@gmail.com>
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>

--- src/scripts/sysconfig/config_fstab.sh
+++ src/scripts/sysconfig/config_fstab.sh
@@ -1,19 +1,20 @@
 
+uuid1=$1
+uuid2=$2
 
 # fstab
-cat > /etc/fstab << "EOF"
-# Begin /etc/fstab
+#cat > /etc/fstab << "EOF"
+echo "# Begin /etc/fstab
 
 # 文件系统     挂载点       类型     选项                转储  检查
 #                                                              顺序
 
-UUID=$1      /            ext4     defaults            1     1
-UUID=$2      /boot        vfat     defaults            1     1
+UUID=${uuid1}     /            ext4     defaults            1     1
+UUID=${uuid2}      /boot        vfat     defaults            1     1
 proc           /proc        proc     nosuid,noexec,nodev 0     0
 sysfs          /sys         sysfs    nosuid,noexec,nodev 0     0
 devpts         /dev/pts     devpts   gid=5,mode=620      0     0
 tmpfs          /run         tmpfs    defaults            0     0
 devtmpfs       /dev         devtmpfs mode=0755,nosuid    0     0
 
-# End /etc/fstab
-EOF
+# End /etc/fstab">/etc/fstab
