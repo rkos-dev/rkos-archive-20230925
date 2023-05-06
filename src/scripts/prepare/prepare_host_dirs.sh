0c37a9702c3ea4db47793ebbd8f6f5c8203a9160
xyyy1420 <xyyy1420@gmail.com>
2023-05-06 20:17:53 +0800

    I made a new fork because the old fork was polluted with a lot of duplicate signature information (#10)
    * feat: add github auto ci
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: add auto work scripts
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update package.json
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update base config
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: delete old config
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update package info
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update utils
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update readme.md
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update make.sh
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update downloader
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update readme
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update host user config
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update package var name
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update package var name
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update main
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update llvm script
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update kernel script
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    * feat: update host prepare
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>
    
    ---------
    
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>

--- src/scripts/prepare/prepare_host_dirs.sh
+++ src/scripts/prepare/prepare_host_dirs.sh
@@ -2,17 +2,16 @@ cd $LFS
 mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 
-mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin} $LFS/root
 
 for i in bin lib sbin; do
   ln -sv usr/$i $LFS/$i
 done
 
 case $(uname -m) in
-  x86_64) mkdir -pv $LFS/lib64 ;;
+x86_64) mkdir -pv $LFS/lib64 ;;
 esac
 
 mkdir -pv $LFS/tools
 
 ln -sv $LFS/tools /
-
