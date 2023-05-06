d4f9ca0e55d2ac67eedbd246eac4589b6768e0ad
xyyy1420 <xyyy1420@gmail.com>
2023-05-06 19:46:37 +0800

    feat: update kernel script
    Signed-off-by: xyyy1420 <xyyy1420@gmail.com>

--- src/scripts/build_rust_support_packages/kernel.sh
+++ src/scripts/build_rust_support_packages/kernel.sh
@@ -1,4 +1,3 @@
-
 rustup component add rust-src
 
 cargo install --locked --version $(scripts/min-tool-version.sh bindgen) bindgen
@@ -7,10 +6,12 @@ rustup component add rustfmt
 
 rustup component add clippy
 
+make CC=clang rustavailable
 
-make CC=clang  rustavailable
+#make CC=clang allnoconfig /config-6.1
+cp /config-6.1 ./.config
 
-make CC=clang allnoconfig /config-6.1
+make CC=clang oldconfig
 
 make CC=clang
 
@@ -21,7 +22,6 @@ cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-6.1-rkos-0.0.1
 cp -v System.map /boot/System.map-6.1
 
 cp -v .config /boot/config-6.1
-
 
 install -d /usr/share/doc/linux-6.1
 cp -r Documentation/* /usr/share/doc/linux-6.1
